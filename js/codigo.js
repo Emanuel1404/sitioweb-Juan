b0ea4d42a3810d38b685ac77b679056727d03dbe
Emanuel1404 <66748889+Emanuel1404@users.noreply.github.com>
2020-12-09 22:31:34 -0500

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,7 @@
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let semaforo = true;
+
+
+document.querySelectorAll(".hamburguesa")[0].addEventListener("click",function(){
+    enlacesHeader.classList.toggle("menudos")
+})
